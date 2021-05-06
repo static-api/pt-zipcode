f0749f7e9a0d3e3423c56e7151e130f09986adcb
Rafael Marmelo <rmarmelo@gmail.com>
2021-05-06 04:13:49 +0100

    Initial version.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pt-zipcode
+
+5c33874f76bdda6795d514ea439a6a20  todos_cp.zip
+
+da33e58c7520ce6e0caf12e2bf426fb0  concelhos.txt
+d9abe17dc29da91b44935e8aaea84688  distritos.txt
+810b4c470a502ec19993c4ffb4229a31  leiame.txt
+f6b1f27ae8519a9b8a81a4f42f053552  todos_cp.txt
---
+++ build.py
@@ -0,0 +1,27 @@
+import csv
+import json
+import os
+ 
+output = 'build'
+source = 'data/todos_cp.txt'
+
+try:
+    os.mkdir(output)
+except:
+    pass
+
+# original field names from leiame.txt
+fieldnames = ('DD', 'CC', 'LLLL', 'LOCALIDADE', 'ART_COD', 'ART_TIPO', 'PRI_PREP', 'ART_TITULO', 'SEG_PREP', 'ART_DESIG', 'ART_LOCAL', 'TROÇO', 'PORTA', 'CLIENTE', 'CP4', 'CP3', 'CPALF')
+
+with open(source, encoding='latin-1') as dataset:
+    csv_reader = csv.DictReader(dataset, fieldnames, delimiter=';')
+
+    for row in csv_reader:
+        zip = f'{row["CP4"]}-{row["CP3"]}'
+        with open(f'{output}/{zip}.json', 'w', encoding='utf-8') as out:
+            out.write(json.dumps(rowca, ensure_ascii=False))
+
+# TODO the same XXXX-XXX has more than 1 result
+# TODO lookup district and region
+# TODO join address
+# TODO cleanup end result
